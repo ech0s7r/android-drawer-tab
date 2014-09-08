c0090d9a9742252d5391367051c55f35a20ff72a
ech0s7r <ech0s7r@gmail.com>
2014-09-08 15:32:29 +0200

    Added SwipeRefreshLayout demo in fragments.

--- src/com/echlabsw/android/drawertab/ExampleFragment.java
+++ src/com/echlabsw/android/drawertab/ExampleFragment.java
@@ -16,14 +16,19 @@
 package com.echlabsw.android.drawertab;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-public class ExampleFragment extends Fragment {
+public class ExampleFragment extends Fragment implements
+		SwipeRefreshLayout.OnRefreshListener {
+
+	private SwipeRefreshLayout mSwipeLayout;
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -34,6 +39,14 @@ public class ExampleFragment extends Fragment {
 		TextView txt = (TextView) rootView
 				.findViewById(R.id.content_fragment_text);
 		txt.setText(getString(R.string.text, b.getInt("index")));
+
+		mSwipeLayout = (SwipeRefreshLayout) rootView
+				.findViewById(R.id.swipe_container);
+		mSwipeLayout.setOnRefreshListener(this);
+		mSwipeLayout.setColorScheme(android.R.color.holo_blue_bright,
+				android.R.color.holo_green_light,
+				android.R.color.holo_orange_light,
+				android.R.color.holo_red_light);
 
 		return rootView;
 	}
@@ -52,6 +65,16 @@ public class ExampleFragment extends Fragment {
 	@Override
 	public void onDestroy() {
 		super.onDestroy();
+	}
+
+	@Override
+	public void onRefresh() {
+		new Handler().postDelayed(new Runnable() {
+			@Override
+			public void run() {
+				mSwipeLayout.setRefreshing(false);
+			}
+		}, 2000);
 	}
 
 }
